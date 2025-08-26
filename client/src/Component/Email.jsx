7ee3799eab6c11bc8f0134ce12fc96a7b05f2b31
zakariaarnop51 <zakariaarnop51@gmail.com>
2025-08-26 06:58:07 +0600

    v1 sinnup

--- client/src/Component/Email.jsx
+++ client/src/Component/Email.jsx
@@ -1,6 +1,8 @@
 import Layout from "../Layout/Layout"
+import UserStore from "../Store/UserStor.js";
 
 function Email() {
+    const { isSubmitting } = UserStore()
     return (
         <Layout>
             <div className='container'>
@@ -9,7 +11,10 @@ function Email() {
                         <h1>Enter Your Email</h1>
                         <p>A verification code will be the email address your provide</p>
                         <input type="text" placeholder='Email address' />
-                        <button className='btn btn-primary'>Send Code</button>
+                        <button className='btn btn-primary'>{isSubmitting ? (<div >
+                            <span className="spinner-border" role="status"></span>
+                            <span className=" visible">Loading...</span>
+                        </div>):(<span>Sine-UP</span>)}</button>
                     </div>
                 </div>
             </div>
--- client/src/Store/UserStor.js
+++ client/src/Store/UserStor.js
@@ -2,11 +2,15 @@ import axios from "axios";
 import { create } from "zustand";
 import { setEmail , getEmail } from "../Utility";
 const UserStore = create((set) => ({
+
+    isSubmitting: false,
     UserOtpReset: async (email) => {
         try {
+            set({ isSubmitting: true });
             const response = await axios.post(`http://localhost:5000/api/v1/UserOtp/${email}`);
              setEmail(email);
             if (response.status === "Success") {
+                set({ isSubmitting: false });
                 return true;
             } else {
                 throw new Error("Failed to send OTP reset");
@@ -18,9 +22,11 @@ const UserStore = create((set) => ({
     },
     UserOtpVerify: async (otp) => {
         try {
+            set({ isSubmitting: true });
             let email = getEmail();
             const response = await axios.post(`http://localhost:5000/api/v1/UserVerifyLogin/${ email, otp }`);
             if (response.status === "Success") {
+                set({ isSubmitting: false });
                 return true;
             } else {
                 throw new Error("Failed to verify OTP");
